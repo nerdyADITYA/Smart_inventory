497f0d0d2fd8b2bc0ea23d271f8372304cc02a0f
Aditya Kadia <adikadia05@gmail.com>
2026-03-08 17:01:48 +0530

    Fix TiDB port

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ const mariadb = require('mariadb');
 require('dotenv').config();
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'localhost', 
-  user: process.env.DB_USER || 'root', 
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'smart_inventory',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 5,
   multipleStatements: true,
   ssl: {
